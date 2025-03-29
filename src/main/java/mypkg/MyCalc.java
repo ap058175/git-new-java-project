e4d01ee8999ea59e12591475507e8f5648231911
ap058175 <ap05817@gmail.com>
2025-03-29 13:36:29 +0530

    diff(),mult() functions are added

--- src/main/java/mypkg/MyCalc.java
+++ src/main/java/mypkg/MyCalc.java
@@ -6,11 +6,23 @@ public class MyCalc {
 	{
 		return a+b;
 	}
-
+	
+	public int diff(int a, int b) 
+	{
+		return a-b;
+	}
+	
+	public int mult(int a, int b) 
+	{
+		return a*b;
+	}
+     
+	
 	public static void main(String[] args) {
 		MyCalc calc=new MyCalc();
 		System.out.println("Sum is :"+calc.sum(20, 30));
-
+		System.out.println("diff is :"+calc.diff(20, 30));
+		System.out.println("mult is :"+calc.mult(20, 30));
 	}
 
 }
