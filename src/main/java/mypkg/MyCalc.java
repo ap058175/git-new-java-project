a53a2a902f3bb48c9c0ec3a4f4f8d66ada59b480
ap058175 <ap058175@gmail.com>
2025-07-07 10:14:10 +0530

    commit changes

--- src/main/java/mypkg/MyCalc.java
+++ src/main/java/mypkg/MyCalc.java
@@ -16,13 +16,16 @@ public class MyCalc {
 	{
 		return a*b;
 	}
-     
-	
+
+	public int greetdemo(){
+		System.out.println("Div is completed, diff is completed , mult is completed");
+	}
 	public static void main(String[] args) {
 		MyCalc calc=new MyCalc();
 		System.out.println("Sum is :"+calc.sum(20, 30));
 		System.out.println("diff is :"+calc.diff(20, 30));
 		System.out.println("mult is :"+calc.mult(20, 30));
+		calc.greetdemo();
 	}
 
 }
